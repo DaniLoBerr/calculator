f8ef4711493ae8fbb52bd3d74de92f8b221f8453
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2024-08-20 09:55:15 +0200

    Create the function that performs the operations
    Create three variables: one for the first operand, one for the operator
    and one for the second operand. They will be used later to update the
    calculator display.
    
    Fix a bug in the add and subtract functions: The initial value
    of the reduce methods was wrong.
    
    Update the nomenclature of the parameters of ths operation functions.

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-function add(...array) {
-  return array.reduce((acc, cur) => acc + cur, 0);
+function add(...operands) {
+  return operands.reduce((acc, cur) => acc + cur);
 }
-function subtract(...array) {
-  return array.reduce((acc, cur) => acc - cur, 0);
+function subtract(...operands) {
+  return operands.reduce((acc, cur) => acc - cur);
 }
-function multiply(...array) {
-  return array.reduce((acc, cur) => acc * cur);
+function multiply(...operands) {
+  return operands.reduce((acc, cur) => acc * cur);
 }
-function divide(...array) {
-  return array.reduce((acc, cur) => acc / cur);
+function divide(...operands) {
+  return operands.reduce((acc, cur) => acc / cur);
+}
+
+let firstOperand;
+let operator;
+let secondOperand;
+
+function operate(firstOperand, operator, secondOperand) {
+  switch(operator) {
+    case "+":
+      return add(firstOperand, secondOperand);
+    case "-":
+      return subtract(firstOperand, secondOperand);
+    case "*":
+      return multiply(firstOperand, secondOperand);
+    case "/":
+      return divide(firstOperand, secondOperand);
+  }
 }
