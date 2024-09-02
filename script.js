02393e463495b9feb91db132d6f91e96ca5a69b1
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2024-09-02 10:42:22 +0200

    Refactor: eval one pair of numbers at a time.
    Give global variables an initial empty or 0 state.
    
    Give a default case to the switch conditional of the "operate" function
    to return 0.
    
    Move the "clear" and "equals" functions aside and refactor their
    contents to perform the desired function.
    
    Refactor the listeners of the number, operation and decimal point
    buttons to evaluate a single pair of numbers at a time.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let displayValue;
-let firstOperand;
-let operator;
-let secondOperand;
+let displayValue = 0;
+let firstOperand = 0;
+let operator = "";
+let secondOperand = 0;
+const display = document.querySelector("#display");
+display.textContent = displayValue;
 
+// Operations
 function add(...operands) {
   return operands.reduce((acc, cur) => acc + cur);
 }
@@ -25,37 +28,71 @@ function operate(firstOperand, operator, secondOperand) {
       return multiply(firstOperand, secondOperand);
     case "/":
       return divide(firstOperand, secondOperand);
+    default:
+      return 0;
   }
 }
+function clear() {
+  display.textContent = 0;
+  displayValue = 0;
+  firstOperand = 0;
+  operator = "";
+  secondOperand = 0;
+}
+function equals() {
+  secondOperand = +displayValue;
+  displayValue = operate(firstOperand, operator, secondOperand);
+  display.textContent = displayValue;
+  firstOperand = +displayValue;
+  operator = "";
+  secondOperand = 0;
+}
 
-const display = document.querySelector("#display");
-
+// Buttons
 const numberButtons = document.querySelectorAll(".number-button");
 numberButtons.forEach(button => {
   button.addEventListener("click", () => {
+    if (displayValue === 0 || operator) display.textContent = "";
     display.textContent += button.id;
+    displayValue = display.textContent;
   })
 });
 
 const pointButton = document.querySelector("#point");
 pointButton.addEventListener("click", () => {
-  display.textContent += pointButton.textContent
+  const pointNumberFilter = displayValue
+    .split("")
+    .filter(element => element === ".")
+    .toString()
+  if (pointNumberFilter === "") {
+    display.textContent += pointButton.textContent;
+    displayValue = display.textContent;
+  }
 });
 
 const operatorButtons = document.querySelectorAll(".operator-button");
 operatorButtons.forEach(button => {
   button.addEventListener("click", () => {
-    firstOperand = +display.textContent;
-    operator = button.textContent;
-    display.textContent = "";
+    if (!firstOperand) {
+      firstOperand = +displayValue;
+      operator = button.textContent;
+      displayValue = 0;
+    } else if (operator === "") {
+      operator = button.textContent;
+    } else {
+      secondOperand = +displayValue;
+      displayValue = operate(firstOperand, operator, secondOperand);
+      display.textContent = displayValue;
+      firstOperand = +displayValue;
+      displayValue = 0;
+      operator = button.textContent;
+      secondOperand = 0;
+    }
   })
 })
 
 const clearButton = document.querySelector("#clear");
-clearButton.addEventListener("click", () => display.textContent = "");
+clearButton.addEventListener("click", () => clear());
 
 const equalsButton = document.querySelector("#equals");
-equalsButton.addEventListener("click", () => {
-  secondOperand = +display.textContent;
-  display.textContent = operate(firstOperand, operator, secondOperand);
-});
+equalsButton.addEventListener("click", () => equals());
